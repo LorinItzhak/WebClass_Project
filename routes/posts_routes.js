ced641a41a6d8994dcf61ad6023db86d7998a545
LorinItzhak <lorinitzhak284@gmail.com>
2024-11-26 20:18:08 +0200

    lorin_routing and connect mongoDB

---
+++ routes/posts_routes.js
@@ -0,0 +1,13 @@
+
+const express= require('express');
+const router = express.Router();
+const post_ = require('../controllers/post_controller');
+
+router.post('/',post_.AddANewPost);
+router.get('/',post_.getAllPost);
+router.get('/:id',post_.getPostById);
+router.delete('/:id', post_.deleteAPost);
+router.put('/:id', post_.updateAPost);
+
+
+module.exports= router;
