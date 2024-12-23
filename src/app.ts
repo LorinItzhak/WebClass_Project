2e94896492e91adccbfa736b2f46d1953dd75eba
LorinItzhak <lorinitzhak284@gmail.com>
2024-12-23 15:52:07 +0200

    reflace the swaggwer

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
+import initApp from "./server";
 
 const port = process.env.PORT;
-const app = express();
 
-// Swagger setup
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// ... your other middleware and route setups
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+initApp().then((app) => {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
 });
  
